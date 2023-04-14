66b744318f8da651ce9c1264d915bdfa1cff7721
Neptu-ne <95061540+Neptu-ne0413@users.noreply.github.com>
2023-04-14 20:56:14 +0900

    Update search.php

--- search.php
+++ search.php
@@ -1,24 +1,25 @@
-<?php
-  $dictionary = array(
-    "りんご" => "apple",
-    "バナナ" => "banana",
-    "チェリー" => "cherry",
-    "ドラゴンフルーツ" => "dragon fruit",
-    "エッグフルーツ" => "egg fruit",
-    "フィグ" => "fig",
-    "グレープフルーツ" => "grapefruit",
-    "ハニーデュー" => "honeydew",
-    "アイスクリームビーン" => "ice cream bean",
-    "ジャックフルーツ" => "jackfruit"
-  );
-
-  $search_word = $_GET["word"];
-
-  if (array_key_exists($search_word, $dictionary)) {
-    $result = "<p>" . $search_word . "の英語は「" . $dictionary[$search_word] . "」です。</p>";
-  } else {
-    $result = "<p>「" . $search_word . "」は辞書にありません。</p>";
-  }
-
-  echo $result;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>英和辞書</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<h1>検索結果</h1>
+		<?php
+		if (isset($_GET['word'])) {
+			$search_word = $_GET['word'];
+			include 'words.php';
+			if (array_key_exists($search_word, $words)) {
+				echo "<p><strong>{$search_word}</strong> : {$words[$search_word]}</p>";
+			} else {
+				echo "<p>該当する単語がありません。</p>";
+			}
+		}
+		?>
+		<a href="index.php">戻る</a>
+	</div>
+</body>
+</html>
