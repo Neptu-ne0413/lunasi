fe5a9a3561ff67ca81d4c065b0c482b0979d6eac
Neptu-ne <95061540+Neptu-ne0413@users.noreply.github.com>
2023-04-14 20:37:33 +0900

    Update search.php

--- search.php
+++ search.php
@@ -1,35 +1,24 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>検索結果</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-  <body>
-    <?php
-      $dictionary = array(
-        "りんご" => "apple",
-        "バナナ" => "banana",
-        "チェリー" => "cherry",
-        "ドラゴンフルーツ" => "dragon fruit",
-        "エッグフルーツ" => "egg fruit",
-        "フィグ" => "fig",
-        "グレープフルーツ" => "grapefruit",
-        "ハニーデュー" => "honeydew",
-        "アイスクリームビーン" => "ice cream bean",
-        "ジャックフルーツ" => "jackfruit"
-      );
+<?php
+  $dictionary = array(
+    "りんご" => "apple",
+    "バナナ" => "banana",
+    "チェリー" => "cherry",
+    "ドラゴンフルーツ" => "dragon fruit",
+    "エッグフルーツ" => "egg fruit",
+    "フィグ" => "fig",
+    "グレープフルーツ" => "grapefruit",
+    "ハニーデュー" => "honeydew",
+    "アイスクリームビーン" => "ice cream bean",
+    "ジャックフルーツ" => "jackfruit"
+  );
 
-      $search_word = $_GET["word"];
+  $search_word = $_GET["word"];
 
-      if (array_key_exists($search_word, $dictionary)) {
-        echo "<p>" . $search_word . "の英語は「" . $dictionary[$search_word] . "」です。</p>";
-      } else {
-        echo "<p>「" . $search_word . "」は辞書にありません。</p>";
-      }
-    ?>
-  </body>
-</html>
+  if (array_key_exists($search_word, $dictionary)) {
+    $result = "<p>" . $search_word . "の英語は「" . $dictionary[$search_word] . "」です。</p>";
+  } else {
+    $result = "<p>「" . $search_word . "」は辞書にありません。</p>";
+  }
+
+  echo $result;
+?>
