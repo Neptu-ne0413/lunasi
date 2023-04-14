771bc7d97427f34ef44767527429e56d96e41c98
Neptu-ne <95061540+Neptu-ne0413@users.noreply.github.com>
2023-04-14 20:26:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
-<?php
-// データベース接続情報
-$dbname = '辞書DB名';
-$host = 'ホスト名';
-$user = 'ユーザー名';
-$pass = 'パスワード';
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>検索結果</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <?php
+      $dictionary = array(
+        "りんご" => "apple",
+        "バナナ" => "banana",
+        "チェリー" => "cherry",
+        "ドラゴンフルーツ" => "dragon fruit",
+        "エッグフルーツ" => "egg fruit",
+        "フィグ" => "fig",
+        "グレープフルーツ" => "grapefruit",
+        "ハニーデュー" => "honeydew",
+        "アイスクリームビーン" => "ice cream bean",
+        "ジャックフルーツ" => "jackfruit"
+      );
 
-// データベース接続
-try {
-	$pdo = new PDO("mysql:dbname={$dbname};host={$host};charset=utf8mb4", $user, $pass);
-} catch (PDOException $e) {
-	exit('データベース接続エラー：' . $e->getMessage());
-}
+      $search_word = $_GET["word"];
 
-// 検索ワードを取得
-$word = $_POST['
+      if (array_key_exists($search_word, $dictionary)) {
+        echo "<p>" . $search_word . "の英語は「" . $dictionary[$search_word] . "」です。</p>";
+      } else {
+        echo "<p>「" . $search_word . "」は辞書にありません。</p>";
+      }
+    ?>
+  </body>
+</html>
